91b0a2ef1ad6021c366ad33e859cf81253b1cac2
Anastasija99 <anastasija.petrovska.5@gmail.com>
2022-03-06 21:32:25 +0100

    Initial setup

---
+++ space-movies/src/main/java/mk/ukim/finki/spacemovies/SpaceMoviesApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.spacemovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceMoviesApplication.class, args);
+	}
+
+}
