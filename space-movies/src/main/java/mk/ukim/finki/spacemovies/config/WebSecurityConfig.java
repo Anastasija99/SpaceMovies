7697e3a034d9f09b3c8cb59a83596e31f58507da
Anastasija99 <anastasija.petrovska.5@gmail.com>
2022-04-11 21:14:28 +0200

    Added ui changes to login page, updated webconfig

--- space-movies/src/main/java/mk/ukim/finki/spacemovies/config/WebSecurityConfig.java
+++ space-movies/src/main/java/mk/ukim/finki/spacemovies/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/assets/**", "/register").permitAll()
+                .antMatchers("/", "/login", "/assets/**", "/register").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
